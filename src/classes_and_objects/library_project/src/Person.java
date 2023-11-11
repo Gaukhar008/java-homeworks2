15cfcf4769f3be5d6bdd9c6365824e72ecf9e0fa
Gaukhar Abdizhapbarova <91333462+Gaukhar008@users.noreply.github.com>
2023-11-11 12:29:18 +0600

    Delete src/classes_and_objects/library_project/src directory

--- src/classes_and_objects/library_project/src/Person.java
+++
@@ -1,6 +0,0 @@
-package src.objects_and_objects.library_project;
-
-public class Person {
-    String name;
-    Role role;
-}
--- src/classes_and_objects/library_project/src/Role.java
+++
@@ -1,5 +0,0 @@
-package src.objects_and_objects.library_project;
-
-public class Role {
-    String role;
-}
